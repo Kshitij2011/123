464ecf9e10c151466fc478fafc00b732e267fb2c
Kshitij2011 <77619958+Kshitij2011@users.noreply.github.com>
2021-09-07 16:17:29 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+noseX = 0;
+noseY = 0;
+difference = 0;
+rightWristX = 0;
+leftWristX = 0;
 function setup(){
     video = createCapture(VIDEO);
     //above thing will turn on the webcam
@@ -17,13 +22,29 @@ function modelLoaded(){
     console.log('PoseNet Started!');
 }
 
+
 function gotPoses(results){
    if(results.length > 0)
    {
        console.log(results);
-   }
+       noseX = results[0].pose.nose.x;
+       noseY = results[0].pose.nose.y;
+       console.log("noseX = " + noseX +"noseY = " + noseY);
+
+       leftWristX = results[0].pose.leftWrist.x;
+       rightWristX = results[0].pose.rightWrist.x;
+       difference =floor(leftWristX - rightWristX);
+
+       console.log("leftWristX = " + leftWristX + "rightWristX =" + rightWristX + "difference =" + difference);
+   }   
 }
 
 function draw(){
-    background('#cc0000');
+    background('#00b300');
+
+    document.getElementById("square_sides").innerHTML = "Width And Height of a Square will be =" + difference + "px";
+
+    fill('gray');
+    stroke('black');
+    square(noseX,noseY,difference);
 }
